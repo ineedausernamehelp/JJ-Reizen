c63216edf8643afad3ef4510f9e0df1cbe3df15c
julian <1203211@student.roc-nijmegen.nl>
2023-05-09 11:28:31 +0200

    css toegevoegd
    ik heb ccs toegevoegd

--- comp/header.php
+++ comp/header.php
@@ -5,10 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styling/style.css" />
 </head>
 <header>
     <nav>
-        
+   <div class="image"> <img src="img/logo.png" alt="logo" width="258" height="103" /></div>   
+   <div class="text"><p> bestemmingen</p><p> Contact</p></div>
+   <div class="logo"></div>
+
     </nav>
 </header>
 
