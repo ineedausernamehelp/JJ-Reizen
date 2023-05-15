02f6078686bc090dcd45c3fc3a8b75a9e0e1f3ec
julian <1203211@student.roc-nijmegen.nl>
2023-05-15 11:56:52 +0200

    footer toegevoegd

---
+++ comp/footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styling/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="footer-container">
+        <footer>
+            <div class="footer-grid">
+                <section class="groot-footer-block">
+                    <p class="footer-tekst">Jake van de Kolk <br> Julian Janssen </p>
+                </section>
+                <section class="klein-footer-block">
+                    <img class="klein-footer-img-block" src="../img/email-logo.png" alt="">
+                    <p class="footer-tekst">JJ-reizenCS@JJ-reizen.com</p>
+                </section>
+                <section class="klein-footer-block">
+                    <a href="policy" class="footer-tekst" id="policy" >our policies</a>
+                </section>
+                <section class="klein-footer-block">
+                    <img class="klein-footer-img-block" src="../img/tele-logo.png" alt="">
+                    <p class="footer-tekst">06 12345678901</p>
+                </section>
+            </div>
+            <section class="footer-img-box">
+                <img src="../img/logo.png" alt="logo" class="footer-img">
+            </section>
+        </footer>
+    </div>
+</body>
+
+</html>
