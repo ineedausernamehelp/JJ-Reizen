1b2ceffcc2e664f3b8134bfce99d90300b4e26dc
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-05-15 10:55:41 +0200

    logo bij de footer toegevoegd

--- comp/footer.php
+++ comp/footer.php
@@ -12,26 +12,30 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,500&display=swap" rel="stylesheet">
 </head>
 
-<body class="footer-container">
-    <footer>
-        <section class="groot-footer-block">
-            <p class="footer-tekst">Jake van de Kolk <br> Julian Janssen  </p>
-        </section>
-        <section class="klein-footer-block">
-            <img class="klein-footer-img-block" src="../img/email-logo.png" alt="">
-            <p class="footer-tekst">JJ-reizenCS@JJ-reizen.com</p>
-        </section>
-        <section>
-        </section>
-        <section class="klein-footer-block">
-            <a href="policy" class="footer-tekst">our policies</a>
-        </section>
-        <section class="klein-footer-block">
-            <img class="klein-footer-img-block" src="../img/tele-logo.png" alt="">    
-            <p class="footer-tekst">06 12345678901</p>
-        </section>
-         
-    </footer>
+<body>
+    <div class="footer-container">
+        <footer>
+            <div class="footer-grid">
+                <section class="groot-footer-block">
+                    <p class="footer-tekst">Jake van de Kolk <br> Julian Janssen </p>
+                </section>
+                <section class="klein-footer-block">
+                    <img class="klein-footer-img-block" src="../img/email-logo.png" alt="">
+                    <p class="footer-tekst">JJ-reizenCS@JJ-reizen.com</p>
+                </section>
+                <section class="klein-footer-block">
+                    <a href="policy" class="footer-tekst" class="footer-tekst">our policies</a>
+                </section>
+                <section class="klein-footer-block">
+                    <img class="klein-footer-img-block" src="../img/tele-logo.png" alt="">
+                    <p class="footer-tekst">06 12345678901</p>
+                </section>
+            </div>
+            <section class="footer-img-box">
+                <img src="../img/logo.png" alt="logo" class="footer-img">
+            </section>
+        </footer>
+    </div>
 </body>
 
 </html>
