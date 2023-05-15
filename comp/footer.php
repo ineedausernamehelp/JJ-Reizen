b4204c486ef03dbdd41143ffefff6906f1611d6c
julian <1203211@student.roc-nijmegen.nl>
2023-05-15 10:08:15 +0200

    header afgemaakt

--- comp/footer.php
+++ comp/footer.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../styling/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,500&display=swap" rel="stylesheet">
-</head>
 
 <body class="footer-container">
-    <footer>
+    <footer >
         <section class="groot-footer-block">
             <p>Jake van de Kolk <br> Julian Janssen  </p>
         </section>
@@ -24,7 +12,7 @@
             <img src="../img/logo.png" alt="logo">
         </section>
         <section class="klein-footer-block">
-            <a href="policy">our policies</a>
+            <a href="../policy.php">our policies</a>
         </section>
         <section class="klein-footer-block">
             <p>06 12345678901</p>
