740fbe5784b9ba12a40e27b3f4fa3cecd77157e9
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-05-16 09:43:37 +0200

    head weggehaald in footer

--- comp/footer.php
+++ comp/footer.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../styling/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com%22%3E/
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,500&display=swap" rel="stylesheet">
-</head>
 
 
     <div class="footer-container">
