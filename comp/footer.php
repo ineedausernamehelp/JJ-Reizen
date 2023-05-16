81b271f1b141da56f25708f7c386600e6b9233f5
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-05-16 09:43:20 +0200

    logo goed in footer gemakat

--- comp/footer.php
+++ comp/footer.php
@@ -20,19 +20,19 @@
                     <p class="footer-tekst">Jake van de Kolk <br> Julian Janssen </p>
                 </section>
                 <section class="klein-footer-block">
-                    <img class="klein-footer-img-block" src="../img/email-logo.png" alt="">
+                    <img class="klein-footer-img-block" src="img/email-logo.png" alt="">
                     <p class="footer-tekst">JJ-reizenCS@JJ-reizen.com</p>
                 </section>
                 <section class="klein-footer-block">
                     <a href="policy" class="footer-tekst" id="policy" >our policies</a>
                 </section>
                 <section class="klein-footer-block">
-                    <img class="klein-footer-img-block" src="../img/tele-logo.png" alt="">
+                    <img class="klein-footer-img-block" src="img/tele-logo.png" alt="">
                     <p class="footer-tekst">06 12345678901</p>
                 </section>
             </div>
             <section class="footer-img-box">
-                <img src="../img/logo.png" alt="logo" class="footer-img">
+                <img src="img/logo.png" alt="logo" class="footer-img">
             </section>
         </footer>
     </div>
