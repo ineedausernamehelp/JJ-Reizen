6a9cbc90cf371e4c99efc8fca8b3a6d4042e0390
julian <1203211@student.roc-nijmegen.nl>
2023-05-23 11:12:24 +0200

    verdergewerkt aan polecy

--- polecy.php
+++ polecy.php
@@ -4,12 +4,19 @@ require_once('comp/header.php');
 <div class="hooftcontainer-polecy">
     <div class="copyright-hooft">
     <div class="copyright-text"><p>copyright</p>
-</div><div id="plusenmin"><p>+</p></div></div>
-    <div class="privacy-hooft">
+    
+</div><div id="plusenmin"><p id="plus1" onclick="plus1()">+</p></div></div>
+<div id="informatie"><p id=text></p></div>
+    <div id="privacy-hooft">
     <div class="privacy-text"><p>privacy</p></div>
-    <div id="plusenmin"><p>+</p></div></div>
+    <div id="plusenmin"><p id="plus2" onclick="plus2()">+</p></div></div>
+
+    <div id="informatie2"><p id=text2></p></div>
 </div>
+
+
 <?php 
 require_once('comp/footer.php')
 ?>
+<script src="scripts/plusmi.js"></script>
 </html>
---
+++ scripts/plusmi.js
@@ -0,0 +1,19 @@
+
+function plus1() {
+    document.getElementById("plus1").innerHTML = "-";
+    document.getElementById("informatie").style= 'background-color: blue;  height: 30vw;     width: 55% ;     margin-bottom: 5%; ';
+ 
+
+}
+
+function plus2() {
+            document.getElementById("plus2").innerHTML = "+";
+            document.getElementById("privacy-hooft").style='margin:0; margin-top: 5%';
+          
+            document.getElementById("informatie2").style= 'background-color: blue;  height: 30vw;     width: 55% ;     margin-bottom: 5%; ';
+
+        
+    
+ }
+    
+
