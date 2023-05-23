6d6ef2adc0b8f0d3d9dff49357db3070915d5d6c
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-05-23 09:14:40 +0200

    add user en admin panel gemaakt

---
+++ admin/add-user.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../styling/style.css" />
+<?Php include_once('../comp/header.php')?>
+<form action="create-reis.php" class="contact-container">
+    <h1>create user</h1>
+    <input type="text" placeholder="username" name="username" class="input-boxes">
+    <input type="text" name="password" placeholder="Password" id="" class="input-boxes">
+    <input type="submit" value="Submit">    
+</form>
---
+++ adminPanel.php
@@ -0,0 +1,10 @@
+<?php include_once('comp/header.php') ?>
+<div class="contact-container">
+    <a href="admin/geboekte-reizen.php">bekijk en verwijder geboekte reizen </a>
+    <a href="admin/create-reis.php">Create nieuwe reizen</a>
+    <a href="admin/update-reis.php">update reizen</a>
+    <a href="admin/delete-reis.php">delete reizen</a>
+    <a href="admin/feedback-back.php">bekijk en verwijder feedback</a>
+    <a href="admin/add-user.php">User toevoegen</a>
+    <a href="admin/del-user.php">User verwijderen</a>
+</div>
