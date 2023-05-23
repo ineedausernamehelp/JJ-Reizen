8d249e392cef20390adfba7c95ef206412ecb4b3
julian <1203211@student.roc-nijmegen.nl>
2023-05-23 09:15:59 +0200

    verder gewerkt aan polecies

--- polecy.php
+++ polecy.php
@@ -3,9 +3,11 @@ require_once('comp/header.php');
 ?>
 <div class="hooftcontainer-polecy">
     <div class="copyright-hooft">
-    <div class="copyright-hooft"></div></div>
-    <div>
-    <div></div></div>
+    <div class="copyright-text"><p>copyright</p>
+</div><div id="plusenmin"><p>+</p></div></div>
+    <div class="privacy-hooft">
+    <div class="privacy-text"><p>privacy</p></div>
+    <div id="plusenmin"><p>+</p></div></div>
 </div>
 <?php 
 require_once('comp/footer.php')
