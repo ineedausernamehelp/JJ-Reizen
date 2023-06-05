0b1dd8716097391653a50a26c246646a604cc1c5
julian <1203211@student.roc-nijmegen.nl>
2023-06-05 11:07:15 +0200

    polecies afgemaakt
    polecies afgemaakt

--- polecy.php
+++ polecy.php
@@ -4,14 +4,171 @@ require_once('comp/header.php');
 <div class="hooftcontainer-polecy">
     <div class="copyright-hooft">
     <div class="copyright-text"><p>copyright</p>
-    
-</div><div id="plusenmin"><p id="plus1" onclick="plus1()">+</p></div></div>
+
+</div><div id="plusenmin"><p id="plus1" onclick="plus1()">+</p>
+
+</div>
+
+</div>
+<div class="copyrightploe" id="copyrightploe">
+<pre>
+
+
+                           © jj-reizen, Julian Janssen en Jake van de Kolk
+
+  When does copyright apply?
+  Copyright exists from the moment you created your work. You do not need to register or file your work. 
+  Copyright is automatic, free-of-charge, and copyright protection applies internationally. 
+  The Dutch Copyright Act only protects your work if it meets the following 3 conditions:
+  Your work is original and personal, it cannot be similar to works of others.
+  Your work is perceptible by senses. This implies that your work can be seen, read, or heard.
+  Your work does not concern a technical product or production process. In this case you must apply for a patent.
+</pre>
+</div>
 <div id="informatie"><p id=text></p></div>
     <div id="privacy-hooft">
     <div class="privacy-text"><p>privacy</p></div>
     <div id="plusenmin"><p id="plus2" onclick="toggleClass()">+</p></div></div>
 
-    <div id="informatie2"><p id=text2></p></div>
+    <div class="privacypole" id="privacypole">
+        <pre>
+                                              PRIVACYVERKLARING
+ 
+  Zodra je onze website jj-reizen bezoekt of contact met ons opneemt, ontvangen we informatie over jou. 
+  In deze privacyverklaring leggen we uit wat we met die informatie doen.
+  We gaan altijd zorgvuldig met je informatie om en slaan die veilig op. 
+  Heb je vragen of wil je weten welke informatie we van je hebben, 
+  neem dan contact op met ons. We kunnen deze privacyverklaring aanpassen als we dat nodig vinden. 
+  We raden je daarom aan om deze privacyverklaring regelmatig te bekijken, 
+  zodat je van deze wijzigingen op de hoogte bent. 
+  Deze privacyverklaring is voor het laatst gewijzigd op 2 juni 2023. 
+  Inhoudsopgave
+  1.	Wanneer pas je deze privacyverklaring toe?
+  2.	Wie gebruikt jouw gegevens?
+  3.	Van wie gebruiken we gegevens?
+  4.	Hoe komen we aan je gegevens?
+  5.	Welke gegevens van jou gebruiken we?
+  6.	Waarvoor gebruiken we je gegevens?
+  7.	Hoelang bewaren we je gegevens?
+  8.	Met wie delen we je gegevens?
+  9.	Waar slaan we je gegevens op?
+  10.	Hoe veilig zijn je gegevens bij ons?
+  11.	Wat mag je van ons vragen?
+  12.	Welke regels gelden voor deze privacyverklaring?
+  13.	Welke cookies gebruiken we?
+  14.	Wat doen we met gegevens van minderjarigen?
+  15.	Heb je een vraag over deze privacyverklaring?
+
+ 
+  1.	Wanneer pas je deze privacyverklaring toe?
+  Deze privacyverklaring is van toepassing op alle persoonsgegevens die we verwerken en op alle domeinen 
+  die aan ons gerelateerd zijn. Het gaat hier om de persoonsgegevens van iedereen die weleens contact met ons 
+  heeft gehad of onze website heeft bezocht, zoals bezoekers, klanten en zakelijke contactpersonen. 
+  Persoonsgegevens zijn alle gegevens die herleidbaar zijn tot jou als individu, 
+  zoals je naam, telefoonnummer, e-mailadres, klantnummer of surfgedrag. Wil je meer weten over persoonsgegevens, 
+  kijk dan op de websitevan de Autoriteit Persoonsgegevens.
+
+ 
+  2.	Wie gebruikt mijn gegevens?
+  JJ-reizen is verantwoordelijk voor de website jj-reizen en daarmee de verantwoordelijke organisatie voor het 
+  gebruik van je persoonsgegevens zoals beschreven in deze privacyverklaring. De volledige gegevens zijn:
+  JJ-reizen Nijegen Hjindaal S825IZ Nijegen Hjindaal 420
+ 
+ 
+  3.	Van wie gebruiken we gegevens? 
+  We verwerken de persoonsgegevens van iedereen die contact met ons heeft gehad of onze website heeft bezocht. 
+  Dit zijn onder meer bezoekers, particuliere klanten, zakelijke klanten en contactpersonen van onze partners.
+ 
+  4.	Hoe komen we aan je gegevens? We krijgen de gegevens rechtstreeks van jou zodra je:
+  •	Onze website bezoekt
+ 
+ 
+  5.	Welke gegevens gebruiken we van je? We maken gebruik van de volgende gegevens:
+  •	Naam
+  •	Geslacht
+  •	Woonadres of vestigingsadres
+  •	Bezorgadres
+  •	E-mailadres
+  •	Telefoonnummer
+  •	Betaalgegevens
+  •	BSN
+  •	Salaris
+  •	Functie
+  •	Financiële gegevens
+
+
+  6.	Waarvoor gebruiken we je gegevens? 
+  We gebruiken je persoonsgegevens alleen voor het doel waarvoor we ze mogen gebruiken:
+  •	We gebruiken je gegevens omdat we je als klant de beste service willen geven en 
+  we dat zonder die informatie niet kunnen doen.
+
+
+  7.	Hoelang bewaren we je gegevens? 
+  We bewaren je persoonsgegevens zo lang als we dat volgens de wet moeten doen en zo lang als 
+  nodig is voor het doel waarvoor we je gegevens gebruiken. Zolang je bijvoorbeeld klant bij ons bent,
+  bewaren we je gegevens volgens de wettelijke bewaartermijn van zeven jaar. Daarna bewaren we je gegevens 
+  alleen voor statistische doeleinden en om eventuele klachten of juridische zaken af te handelen. 
+  Wil je meer weten over hoelang we specifieke gegevens van je bewaren, neem dan contact met ons op.
+
+
+  8.	Met wie delen we je gegevens?
+  We kunnen je persoonsgegevens delen met anderen. We doen dit alleen als:
+  •	Je klant bij ons bent.
+
+
+  9.	Waar slaan we je gegevens op? We verwerken je gegevens binnen de Europese Economische Ruimte (EER). 
+  Dit houdt in dat we ook je gegevens binnen de EER opslaan. Heb je hierover vragen, 
+  neem dan gerust contact met ons op.
+
+
+  10.	Hoe veilig zijn je gegevens bij ons? 
+  We hebben er veel aan gedaan om je gegevens zowel organisatorisch als technisch zo goed mogelijk te 
+  beveiligen. We hebben onze systemen en verschillende communicatiemiddelen beveiligd om 
+  ervoor te zorgen dat je gegevens niet in de handen van anderen terechtkomen. Je gegevens zijn dus veilig bij ons. 
+  Ook zorgen we ervoor dat je gegevens alleen worden gebruikt door mensen die daar van ons toestemming voor hebben 
+  gekregen. Heb je vragen over de specifieke manier van beveiligen, neem dan contact met ons op. 
+  Wanneer we je gegevens  delen met anderen,dan zijn zij ook verplicht om daar met dezelfde zorg mee om
+  te gaan als wij. Merk je dat dit niet zo is, laat het ons ook weten.
+
+
+  11.	Wat mag je van ons vragen? 
+  Omdat we persoonsgegevens van je gebruiken, heb je verschillende rechten. Deze rechten zetten we 
+  hieronder voor je op een rijtje.
+  •	Recht: Julie hebben geen rechten op deze site.
+
+
+  12.	Welke regels gelden voor deze privacyverklaring? 
+  Onze privacyverklaring moet aan verschillende voorwaarden voldoen. Deze voorwaarden vind je met 
+  name terug in de Algemene Verordening Gegevensbescherming. Daarnaast zijn de algemene regels die 
+  volgens de Nederlandse wet gelden van toepassing op 
+  onze privacyverklaring.
+
+
+  13.	Welke cookies gebruiken we? 
+  Je kunt zelf aangeven welke gegevens we van je mogen gebruiken. Heb je ons toestemming gegeven 
+  voor het personaliserenvan je profiel op basis van je surf- en zoekgedrag,
+  dan kunnen we onze website speciaal op jou instellen zodat het gebruik ervan makkelijker en persoonlijker wordt. 
+  We doen dit met behulp van cookies. 
+  Een cookie is een klein tekstbestand dat bij je bezoek aan onze website geplaatst wordt op je hardware.
+  We gebruiken de volgende soorten cookies op onze website:
+  •	Functionele cookies, zoals sessie- en logincookies voor het bijhouden van sessie- en inloginformatie.
+  •	Tracking cookies, zoals advertentiecookies die zijn bedoeld voor het tonen van relevante advertenties.
+
+
+  14.	Wat doen we met gegevens van minderjarigen? 
+  We richten ons met onze website of als organisatie niet specifiek op minderjarigen. Dit houdt in dat als
+  je jonger bent dan 18 jaar, je toestemming nodig hebt van een ouder of voogd om gebruik te maken van onze website. 
+  Ben je bij je bezoek aan onze website of webshop minderjarig, 
+  dan gaan we ervan uit dat je voor je bezoek deze toestemming hebt verkregen.
+
+
+  15.	Heb je een vraag over deze privacyverklaring? 
+  Heb je een vraag over onze privacyverklaring? Neem dan gerust contact met ons op door een mail te sturen naar 
+  vandekolkjake@gmail.com. We helpen je graag.
+
+
+        </pre>
+    </div>
 </div>
 <div class="pad"></div>
 
--- scripts/plusmi.js
+++ scripts/plusmi.js
@@ -2,12 +2,14 @@
 function plus1() {
   
     const plus1 = document.getElementById("plus1");
-    const info1 = document.getElementById("informatie");
-    info1.classList.toggle('hide')
+    const info1 = document.getElementById("copyrightploe");
+    
     if (plus1.innerHTML === "+"){
         plus1.innerHTML = "-";
+        info1.style.display = "block";
     }else{
         plus1.innerHTML = "+";
+        info1.style.display = "none";
     }
 
 
@@ -17,16 +19,18 @@ function plus1() {
 function toggleClass() {
         
         const plus2 = document.getElementById("plus2");
-          const info2 = document.getElementById("informatie2");
-          info2.classList.toggle('hide')
+          const info2 = document.getElementById("privacypole");
+
     if (plus2.innerHTML === "+"){
         plus2.innerHTML = "-";
+        info2.style.display = "block";
     }else{
         plus2.innerHTML = "+";
+        info2.style.display = "none";
     }
-    info2.innerHTML = "test";
+    
 
-        
+   
     
  }
     
