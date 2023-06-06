777fb2e664d2850ad2b304a5c065866813359368
julian <1203211@student.roc-nijmegen.nl>
2023-06-06 09:07:18 +0200

    over-ons bijgewerkt

--- over-ons.php
+++ over-ons.php
@@ -4,14 +4,15 @@ require_once('comp/header.php');
 <div class="achtergrondblock">
 <div class="over-site">
 <div class="logo-over"><img src="img/logo.png" alt="logo" width="600" height="238.94" /></div>
-<div class="info-site">  </div>
+<div class="info-site"><h1.>onze site</h1>
+<div class="textinfo"><p.>wabadcjfoksapj</p> </div></div>
 </div>
 <div class="over-jake" >
 <div class="info-jake"></div>
-<div class="foto-jake"></div>
+<div class="foto-jake"><img src="img/your-poletics-bore-me.png" alt="logo" width="464" height="438" /></div>
 </div>
 <div class="over-julian">
-<div class="foto-julian"></div>
+<div class="foto-julian"><img src="img/ik-in-cirke.png" alt="logo" width="464" height="438" /></div>
 <div class="info-julian"></div>
 </div>
 </div>
