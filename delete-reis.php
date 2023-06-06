b12828f545d574da99e2f0895f8b4a7c0b36aa90
ineedausernamehelp <54716860+ineedausernamehelp@users.noreply.github.com>
2023-06-06 11:28:19 +0200

    delete en reizen inzien afgemaakt

--- delete-reis.php
+++ delete-reis.php
@@ -1,3 +1,16 @@
-<link rel="stylesheet" href="../styling/style.css" />
-<?Php include_once('../comp/header.php')?>
-<?Php include_once('../comp/PDO.php')?>
+<?php include_once("comp/PDO.php");
+$id = $_GET["id"];
+$stmt = $conn ->prepare("SELECT * FROM reizen WHERE id=:id");
+$stmt->execute(['id' => $id]); 
+$user = $stmt->fetch();
+
+
+
+
+    $sql = "DELETE FROM reizen WHERE id  = :id";
+    $stmtt = $conn->prepare($sql);
+    $stmtt->execute(['id' => intval($id)]);
+    header("Location: reizen-inzien.php");
+?>
+
+
--- update-reis.php
+++ update-reis.php
@@ -1,41 +1,65 @@
 
-<?Php include_once('comp/header.php');
-if(isset($_POST['submit'])){
-    $bestemming = (isset($_POST['bestemming']) ? $_POST['bestemming'] : '');
-    $prijs = (isset($_POST['prijs']) ? $_POST['prijs'] : '');
-    $beschrijving = (isset($_POST['beschrijving']) ? $_POST['beschrijving'] : '');
-    $id = (isset($_POST['id']) ? intval($_POST['id']) : 0);
+<?php
+include_once('comp/header.php');
+$id = $_GET["id"];
+//$stmt = $conn ->prepare("SELECT landen.country_id as landen_id, landen.name AS land_naam, places.id AS place_id, places.name AS place_naam, reizen.id as reizen_id, reizen.prijs AS prijs, landen.beschrijving as land_beschrijving FROM `reizen` \n"
+
+//. "\n"
+
+//. "INNER JOIN landen ON reizen.land_id = landen.country_id \n"
+
+//. "INNER JOIN places ON reizen.place_id = places.id;");
+//$stmt->execute(['reizen_id' => $id]); 
+//$user = $stmt->fetch();
+$data = $conn->query("SELECT landen.country_id as landen_id, landen.name AS land_naam, places.id AS place_id, places.name AS place_naam, reizen.id as reizen_id, reizen.prijs AS prijs, reizen.beschrijving AS beschrijving FROM `reizen` \n"
+
+. "\n"
+
+. "INNER JOIN landen ON reizen.land_id = landen.country_id \n"
+
+. "INNER JOIN places ON reizen.place_id = places.id;")->fetch();
+
+
+    if (isset($_POST['edit'])) {
+        $land_naam = (isset($_POST['land_naam']) ? $_POST['land_naam'] : '');
+        $place_naam = (isset($_POST['place_naam']) ? $_POST['place_naam'] : '');
+        $prijs = (isset($_POST['prijs']) ? $_POST['prijs'] : '');
+        $bescrhijving = (isset($_POST['beschrijving']) ? $_POST['beschrijving'] : '');
 
 
 
-    if($_POST['bestemming'] == "" || $_POST['prijs'] == "" || $_POST['beschrijving'] == ""){
-
+        if ($_POST['prijs'] == "" || $_POST['beschrijving'] == "") {
+        } else {
+            $sql = "UPDATE reizen SET prijs=?, beschrijving=? WHERE id = ?";
+            $stmtt = $conn->prepare($sql);
+            $stmtt->execute([$prijs, $bescrhijving, $id]);
+        }
     }
-    else{
-        $sql = "UPDATE reizen SET bestemming=?, prijs=?, beschrijving=? WHERE id = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([$bestemming, $prijs, $beschrijving, $id]);
-    }
 
-}
+//foreach ($data as $row){
+    $landen = $conn->query("SELECT * from landen")->fetchall();
+    $places = $conn->query("SELECT * from places")->fetchAll();
 
-
-
-
-
-
-$data = $conn->query("SELECT * FROM reizen")->fetchAll();
 ?>
 
-<?php foreach ($data as $row){ ?>
-        <form action="update-reis.php" class="contact-container" method="post">
-        <h1>Update reis</h1>
-        <input type="hidden" name="id" value="<?php echo $row['id'] ?>">
-        <input value="<?Php echo $row['bestemming']?>" type="text" placeholder="bestemming" name="bestemming" class="input-boxes">
-        <input value="<?php echo $row['prijs'] ?>" type="text" name="prijs" placeholder="prijs" id="" class="input-boxes">
-        <textarea  name="beschrijving" id="message"  cols="30" rows="10" class="input-boxes groot-input-box" placeholder="<?php echo $row['beschrijving'] ?>"></textarea>
-        <input type="submit" value="Submit" name="submit">    
-        </form>
-    <?php }?>
-
-    <script src='scripts/create-reis.js'>
+<form class="contact-container" method="post">
+    <h1>edit reis</h1>
+    <select id="countries">
+        <?php 
+        foreach($landen as $land) { ?>
+            <option value="<?php echo $land['country_id']?>" <?php echo ($data['landen_id'] == $land['country_id']? 'selected="selected"' : "");?>><?php echo $land['name']?></option>
+        <?php }?>
+    </select>
+    <select id="places">
+        <?php 
+        foreach($places as $place) { ?>
+            <option value="<?php echo $place['id']?>" <?php echo ($data['place_id'] == $place['id']? 'selected="selected"' : "");?>><?php echo $place['name']?></option>
+        <?php }?>
+    </select>
+    <input type="text" name="prijs" id="password" class="input-boxes" value="<?php echo $data["prijs"]; ?>">
+    <input type="text" name="beschrijving" id="password" class="input-boxes" value="<?php echo $data["beschrijving"]; ?>">
+    <input type="hidden" name="id" value="<?php echo $data["reizen_id"]; ?>">
+    <input type="submit" value="Submit" name="edit" id="logSubmit">
+</form>
+<?php //}?>
+<script src="scripts/login.js"> </script>
