84e3886ce79282ac8a94bfbefdf41b4552a2740b
julian <1203211@student.roc-nijmegen.nl>
2023-06-06 09:25:45 +0200

    over ons afgemaakt

--- over-ons.php
+++ over-ons.php
@@ -5,15 +5,22 @@ require_once('comp/header.php');
 <div class="over-site">
 <div class="logo-over"><img src="img/logo.png" alt="logo" width="600" height="238.94" /></div>
 <div class="info-site"><h1.>onze site</h1>
-<div class="textinfo"><p.>wabadcjfoksapj</p> </div></div>
+<div class="textinfo"><p.>wij van jj-reizen hebben deze site gemaakt voor onze vliegtuigmaatschapij. waar wij de co-founders (Julian Janssen en Jake van de Kolk. wij hebben deze site gemaakt in 2023 in het zelfde jaar waarin we dit bedrijf opgezet hadden. en daarom kan wij met trots deze website an u laaten zien. </p> </div></div>
 </div>
 <div class="over-jake" >
-<div class="info-jake"></div>
+<div class="info-jake"><h1>Jake van de kolk</h1>
+<div class="textinfo"><p>Ik ben Jake, een Coeigenaar van JJ reizen. Ik ben gepassioneerd en het blij maken van families  door middel  van hun vakanties voor hun plannen.  Laten we samenwerken om een geweldige vakantie te plannen.
+    
+</p></div>
+</div>
 <div class="foto-jake"><img src="img/your-poletics-bore-me.png" alt="logo" width="464" height="438" /></div>
 </div>
 <div class="over-julian">
 <div class="foto-julian"><img src="img/ik-in-cirke.png" alt="logo" width="464" height="438" /></div>
-<div class="info-julian"></div>
+<div class="info-julian"><h1>Julian Janssen</h1>
+<div class="textinfo"><p>Ik ben Julian, een Coeigenaar van JJ reizen. Ik ben gepassioneerd en het blij maken van families  door middel  van hun vakanties voor hun plannen.  Laten we samenwerken om een geweldige vakantie te plannen.
+</p></div>
+</div>
 </div>
 </div>
 
