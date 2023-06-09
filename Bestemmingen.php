e4b772e2ebe77cf0a9d8aa033c1068e5116441f1
julian <1203211@student.roc-nijmegen.nl>
2023-06-09 13:20:51 +0200

    bestemingen block gemaakt

---
+++ Bestemmingen.php
@@ -0,0 +1,28 @@
+<?php
+require_once('comp/header.php');
+?>
+<div class="bestemmingen-blok">
+<div class="bestemmingen-best1">
+    <div class="best1-mainblok">
+        <h1>naam besteling</h1>
+        <div class="best1-text"> </div>
+            <div class="best1-sijlblok">
+            <div class="best1-euro"> </div>
+            <div class="best1-nu-boek">
+                
+
+                </div>
+                <div>
+
+                </div>
+           
+        </div>
+   </div>
+   <img  class="bast1-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9WYxJ091Vav9hqxR6bDi7tew5RebhFvhgZA&usqp=CAU
+    " alt="best1-img" width="550px" height="350px" />
+</div>
+
+
+
+
+
